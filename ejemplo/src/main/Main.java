689c13813b16589d574ed2650d2bfd164685a130
XPATHER <octavioalcalde2@gmail.com>
2022-05-20 04:07:17 -0300

    version 4

--- ejemplo/src/main/Main.java
+++ ejemplo/src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
         public static void main(String args[]){
-            System.out.println("Hello Java 1");
+            System.out.println("Hello Java 65");
         }
 }
 
